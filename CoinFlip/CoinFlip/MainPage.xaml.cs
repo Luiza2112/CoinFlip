20df6563d1edf8b2d3d5a3dd9691023ff4cad0d2
Luiza2112 <ritafilhotes4@gmail.com>
2024-04-26 17:06:04 -0300

    Chore: Initial commit.

---
+++ CoinFlip/CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CoinSidesPicker.SelectedIndex = 0;
+        }
+
+        private void FlipCoinButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            int coinSide = Convert.ToString(CoinSidesPicker.SelectedItem);
+            int ladoSorteado = new Random().Next(1, selectedSides + 1);
+
+            if (coinSide == "Cara")
+            {
+
+            };
+
+        }
+    }
+
+}
