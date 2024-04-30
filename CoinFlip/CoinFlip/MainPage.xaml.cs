5ef0d4b8a6665452cf278de9a690772ad95166e9
Luiza2112 <ritafilhotes4@gmail.com>
2024-04-30 16:16:47 -0300

    :sparkles:Feat: Lógica terminada.

--- CoinFlip/CoinFlip/MainPage.xaml.cs
+++ CoinFlip/CoinFlip/MainPage.xaml.cs
@@ -12,13 +12,32 @@
 
         private void FlipCoinButton_Clicked(System.Object sender, System.EventArgs e)
         {
-            int coinSide = Convert.ToString(CoinSidesPicker.SelectedItem);
-            int ladoSorteado = new Random().Next(1, selectedSides + 1);
+            string coinSide = Convert.ToString(CoinSidesPicker.SelectedItem);
+            int ladoSorteado = new Random().Next(2);
+            // retorna um número inteiro não negativo aleatório, menor que 2. Ou seja, 0 ou 1.
+            //0 é Cara e 1 é Coroa
 
-            if (coinSide == "Cara")
+
+            if (coinSide == "Cara" && ladoSorteado == 0)
             {
-
-            };
+                CoinImage.Source = "cara.jpeg";
+                TextResult.Text = "O resultado foi Cara, você ganhou!";
+            } 
+            else if(coinSide == "Cara" && ladoSorteado == 1)
+            {
+                CoinImage.Source = "coroa.jpeg";
+                TextResult.Text = "O resultado foi Coroa, você perdeu!";
+            }
+            else if(coinSide == "Coroa" && ladoSorteado == 0)
+            {
+                CoinImage.Source = "cara.jpeg";
+                TextResult.Text = "O resultado foi Cara, você perdeu!";
+            } 
+            else if(coinSide == "Coroa" && ladoSorteado == 1)
+            {
+                CoinImage.Source = "coroa.jpeg";
+                TextResult.Text = "O resultado foi Coroa, você ganhou!";
+            }
 
         }
     }
