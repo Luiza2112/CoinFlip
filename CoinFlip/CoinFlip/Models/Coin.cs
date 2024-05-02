d2ba2109e8f923e15e15d83b230ac9747194557e
Luiza2112 <ritafilhotes4@gmail.com>
2024-05-02 12:39:19 -0300

    :sparkles:Feat: Adicionada a classe Coin.

---
+++ CoinFlip/CoinFlip/Models/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip.Models
+{
+    public class Coin
+    {
+        public string Lado { get; set; }
+        public string LadoEscolhido { get; set; }
+
+        //Construtor tem sempre o nome da classe
+        public Coin()
+        {
+
+        }
+
+        public string Jogar()
+        {
+            //Operador Ternario ? -> if resumido
+            return (new Random().Next(2) == 0) ? "Cara" : "Coroa";
+        }
+
+    }
+}
